05153e1f52a2a68ec661ffde23d18d71a0b6ac8a
owenoconnell1 <131276489+owenoconnell1@users.noreply.github.com>
2026-09-15 13:16:49 -0400

    Edited vite.config for new gh pages base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "gh-pages" ? "/final-project-owen-oconnell/" : "/",
+  base: mode === "gh-pages" ? "/React-Game-Hub/" : "/",
   plugins: [react()],
   server: { port: 5173, open: false },
   preview: { port: 5173 },
